b68dbec7626c4237d81dd83c8bccf3b9b8baca43
Olvea16 <olvea16@student.sdu.dk>
2021-08-05 02:23:57 +0200

    Trying to get the new structure to work

--- fourierart/gui/components/fieldrange.py
+++ fourierart/gui/components/fieldrange.py
@@ -1,4 +1,4 @@
-from fourierart import Range, ParameterInterpolator
+from fourierart import Range, ParameterInterpolator, Nop
 from fourierart.gui.components.field import CustomField
 
 from PyQt5.QtCore import Qt
@@ -6,18 +6,20 @@ from PyQt5.QtWidgets import QGroupBox, QHBoxLayout
 from qtrangeslider import QRangeSlider
 
 class CustomRange(QGroupBox):
-    def __init__(self, title, range: Range, interpolator: str = 'lin', interpolator_order: float = 50):
+    def __init__(self, title, range: Range, interpolator: str = 'lin', interpolator_order: float = 50, input_width: int = 80):
         super().__init__()
 
+        # Initializing attributes
         self.interpolator = ParameterInterpolator(parameter=range, interpolator=interpolator, interpolator_order=interpolator_order)
+        self.callback = Nop # CustomRange and CustomSlider do not pass the value on callback. This has to be read from CustomRange.range or CustomSlider.parameter.
 
+        # Layout
         self.setTitle(title)
-
         self.layout = QHBoxLayout()
         self.setLayout(self.layout)
 
         # Slider
-        self.slider = QRangeSlider(Qt.Vertical)
+        self.slider = QRangeSlider(Qt.Vertical) # TODO: Implement Horizontal.
         slider_value = tuple(self.interpolator.to_slider(v) for v in self.p.get())
         self.update_parameter(slider_value)
         self.slider.sliderMoved.connect(self.on_slider_change)
@@ -26,15 +28,17 @@ class CustomRange(QGroupBox):
 
         # Input field
         self.input = CustomField('')
-        self.input.setFixedWidth(80)
+        self.input.setFixedWidth(input_width)
         self.input.returnPressed.connect(self.on_input_change)
         self.layout.addWidget(self.input)
 
-    def callback(self, value):
-        raise Exception('Not implemented.')
 
     def on_slider_change(self, value):
         raise Exception('Not implemented.')
 
+        self.callback()
+
     def on_input_change(self):
         raise Exception('Not implemented.')
+
+        self.callback()
